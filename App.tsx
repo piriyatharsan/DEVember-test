fd43d493fd606fff972bd907c53e21e01654e4fc
Pirijatharsan <86178608+piriyatharsan@users.noreply.github.com>
2023-12-02 12:08:47 +0530

    my second commit

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
